3bcf8affe9bb4b290a7030848ca411b1c2f45252
sx sx <1049353754@qq.com>
2024-01-31 17:39:52 +0800

    small bug fix

--- game/urls/index.py
+++ game/urls/index.py
@@ -4,6 +4,6 @@ from game.views.index import index
 urlpatterns = [
     path("", index, name="index"),
     path("menu/", include("game.urls.menu.index")),
-    path("playground/", incldue("game.urls.playground.index")),
-    path("settings/", incldue("game.urls.settings.index")),
+    path("playground/", include("game.urls.playground.index")),
+    path("settings/", include("game.urls.settings.index")),
 ]
